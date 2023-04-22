446f9894b3a0b999e8431a337ea63b34fcd0cf66
Wonsup Yoon <pusnow@kaist.ac.kr>
2023-04-22 16:56:10 +0900

    fix

--- tools/add-userdic.ps1
+++ tools/add-userdic.ps1
@@ -4,28 +4,28 @@ $MECAB_EXEC_PATH = "@BIN_PATH@\mecab.exe"
 $DICT_INDEX = "@BIN_PATH@\mecab-dict-index.exe"
 
 function Get-Userdics {
-    $Dir = Get-Childitem $USERDIC_PATH -recurse 
-    $List = $Dir | Where-Object {$_.extension -eq ".csv"} 
+    $Dir = Get-Childitem $USERDIC_PATH -recurse
+    $List = $Dir | Where-Object {$_.extension -eq ".csv"}
     $List
 }
 
 function Get-Cost {
     $input_dic = $args[0]
-    & $DICT_INDEX -m  "$($DIC_PATH)model.def" -d $DIC_PATH -u "$($DIC_PATH)user-$($input_dic)" -f utf-8 -t utf-8 -a "$($USERDIC_PATH)$($input_dic)"
+    & $DICT_INDEX -m "$($DIC_PATH)/model.def" -d $DIC_PATH -u "$($DIC_PATH)/user-$($input_dic)" -f utf-8 -t utf-8 -a "$($USERDIC_PATH)/$($input_dic)"
 }
 
 function Compile {
-    Remove-Item $($DIC_PATH)\*.bin
-    Remove-Item $($DIC_PATH)\*.txt
+    Remove-Item "$($DIC_PATH)/*.bin"
+    Remove-Item "$($DIC_PATH)/*.txt"
     & $DICT_INDEX -d $DIC_PATH -o $DIC_PATH -f UTF-8 -t UTF-8
 
 }
 
 function main {
     Write-Output "generating userdic..."
-    Remove-Item $($DIC_PATH)\user-*.csv
+    Remove-Item "$($DIC_PATH)/user-*.csv"
 
-    Get-Userdics  | ForEach-Object {
+    Get-Userdics | ForEach-Object {
         Get-Cost $_.Name
     }
 
