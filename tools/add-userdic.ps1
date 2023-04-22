e90e318e1ad0bf5d35e1d0a71703a3263746afca
Wonsup Yoon <pusnow@kaist.ac.kr>
2023-04-22 16:45:47 +0900

    fix

--- tools/add-userdic.ps1
+++ tools/add-userdic.ps1
@@ -1,7 +1,7 @@
 $DIC_PATH = "@DIC_PATH@"
-$USERDIC_PATH = "@DIC_PATH@\\user-dic"
-$MECAB_EXEC_PATH = "@BIN_PATH@\\mecab.exe"
-$DICT_INDEX = "@BIN_PATH@\\mecab-dict-index.exe"
+$USERDIC_PATH = "@DIC_PATH@\user-dic"
+$MECAB_EXEC_PATH = "@BIN_PATH@\mecab.exe"
+$DICT_INDEX = "@BIN_PATH@\mecab-dict-index.exe"
 
 function Get-Userdics {
     $Dir = Get-Childitem $USERDIC_PATH -recurse 
@@ -17,7 +17,7 @@ function Get-Cost {
 function Compile {
     Remove-Item $($DIC_PATH)\*.bin
     Remove-Item $($DIC_PATH)\*.txt
-    & $DICT_INDEX -d mecab-ko-dic -o mecab-ko-dic -f UTF-8 -t UTF-8
+    & $DICT_INDEX -d $DIC_PATH -o $DIC_PATH -f UTF-8 -t UTF-8
 
 }
 
